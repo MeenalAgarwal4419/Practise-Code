5117fc7f8ba91c1e1e553fe622dd2823c11a0372
MeenalPrinzu <52129659+MeenalPrinzu@users.noreply.github.com>
2019-06-23 20:02:08 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="url_shortener_pm",
+    version="0.1.1",
+    author=["Prinzu Choudhury","Meenal Agarwal"],
+    author_email="prinzupallab@gmail.com",
+    description="A package to shorten the url",
+    long_description="ShortStr is a Python module to generate distinct, homoglyph-less 'shortstrings' for URL shortners and similar services",
+    long_description_content_type="text/markdown",
+    url="https://github.com/MeenalPrinzu/url_shortner_pm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
